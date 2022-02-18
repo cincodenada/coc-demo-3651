9ea7adf12b698f528390843c6197eba049a0187c
Joel Bradshaw <joelbr@tailormed.co>
2022-02-18 11:06:09 -0800

    Use example file to show what situation we're in

--- demos.sh
+++ demos.sh
@@ -1,4 +1,8 @@
+echo "vim without .vimrc" > dir_a/file
 vim -u NONE -S demo.vim
+echo "vim with coc.nvim" > dir_a/file
 vim -u mini.vim -S demo.vim
+echo "nvim without .vimrc" > dir_a/file
 nvim -u NONE -S demo.vim
+echo "nvim with coc.nvim" > dir_a/file
 nvim -u mini.vim -S demo.vim
